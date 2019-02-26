65e99c64d571c78353c995d43b84615bd5e47527
wtc4863 <wtc4863@rit.edu>
2019-02-26 17:31:05 -0500

    Added basic classes and data members for Book, Visitor, Visit

---
+++ src/Book.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Book {
+
+  private int isbn;
+  private String title;
+  private ArrayList<String> authors;
+  private String publisher;
+  private String datePublished;
+  private int pageCount;
+
+}
---
+++ src/Visit.java
@@ -0,0 +1,8 @@
+public class Visit {
+
+  private Visitor visitor;
+  /*
+  TODO: data members for time in, time out, date
+   */
+
+}
