5c2d72ffe101807acc8296006e40f805ba764800
Austin <ath9902@g.rit.edu>
2019-03-03 18:32:21 -0500

    doc strings added to book

--- src/java/Model/Book.java
+++ src/java/Model/Book.java
@@ -18,6 +18,17 @@ public class Book implements Serializable {
     private Date dateBorrowed;
     private Date dueDate;
 
+    /**
+     * Constructor for book
+     *
+     * @param isbn
+     * @param title
+     * @param authors
+     * @param publisher
+     * @param datePublished
+     * @param pageCount
+     * @param iD
+     */
     public Book(long isbn, String title, String authors, String publisher, String datePublished, int pageCount, int iD) {
         this.isbn = isbn;
         this.title = title;
@@ -28,35 +39,67 @@ public class Book implements Serializable {
         this.tempID = iD;
     }
 
+    /**
+     * gets book Isbn number
+     * @return
+     */
     public long getIsbn() {
         return isbn;
     }
 
+    /**
+     * gets the book title
+     * @return
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * gets the book publish date
+     * @return
+     */
     public String getDatePublished() {
         return datePublished;
     }
 
+    /**
+     * gets list of authors
+     * @return
+     */
     public ArrayList<String> getAuthorsAsList() {
         return (ArrayList<String>) Arrays.asList(authors.split(","));
     }
 
+    /**
+     * format of a purchase
+     * @return
+     */
     public String purchaseFormat() {
         return String.format("%s,%s,%s,%s", isbn, title, authors, Library.DATE_FORMAT.format(datePublished));
     }
 
+    /**
+     * format of a search
+     * @return
+     */
     public String searchFormat() {
         return String.format("%d,%d,%s,%s,%s", tempID, isbn, title, authors,
                 Library.DATE_FORMAT.format(datePublished));
     }
 
+    /**
+     * format of a query
+     * @return
+     */
     public String queryFormat() {
         return String.format("%d,%d,%s,%s", tempID, isbn, title, Library.DATE_FORMAT.format(dateBorrowed));
     }
 
+    /**
+     * gets book temp id
+     * @return
+     */
     public int getTempID() {
         return tempID;
     }
