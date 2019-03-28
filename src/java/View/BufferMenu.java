76d9bb6edd6470a2981b241fb866fc7f7058ef24
Zack Evans <zackalta15@gmail.com>
2019-03-28 17:38:44 -0400

    Code

---
+++ src/java/View/BufferMenu.java
@@ -0,0 +1,121 @@
+package view;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BufferMenu extends JPanel
+{
+    // Creates hash map to store all main panels <Name of Panel,Panel Object>
+    private Map<String, JPanel> mapPanels = new HashMap<>();
+    public static String currentPanel = "MAIN_MENU";
+    public static String lastPanel = "MAIN_MENU";
+    private MainMenu mainMenu;
+    private LoginMenu loginMenu;
+    private SignupMenu signupMenu;
+
+    public BufferMenu()
+    {
+        super();
+        initialize();
+    }
+
+    /**
+     * Function: initialize
+     *
+     * Calls methods to create panel and components
+     */
+
+    public void initialize()
+    {
+        createComponents();
+        initializePanels();
+        addComponents();
+        setDefaults();
+        setOpaque(false);
+    }
+
+    /**
+     * Function: createComponents
+     *
+     * initialize object
+     * Add panel object in to hashmap (mapPanels)
+     */
+
+    public void createComponents()
+    {
+        // Main Menu
+        mainMenu = new MainMenu(this);
+        mapPanels.put("MAIN_MENU", mainMenu);
+
+        loginMenu = new LoginMenu(this);
+        mapPanels.put("LOGIN_MENU", loginMenu);
+
+        signupMenu = new SignupMenu(this);
+        mapPanels.put("SIGNUP_MENU", signupMenu);
+    }
+
+    /**
+     * Function: initializePanels
+     *
+     * Initialize (create) all Panels
+     */
+
+    public void initializePanels()
+    {
+        this.setBorder(new LineBorder(Color.BLUE));
+        //mainMenu.initialize();
+    }
+
+    /**
+     * Function: addComponents
+     *
+     * add all main panel components to panel (Separated in categories)
+     */
+
+    public void addComponents()
+    {
+        // Main Menu
+        add(mainMenu);
+        add(loginMenu);
+        add(signupMenu);
+    }
+
+    /**
+     * Function: setDefaults
+     *
+     * Check to see if there is a password in the database (getPassExist)
+     * Show login panel or main menu at start of the program
+     */
+
+    public void setDefaults()
+    {
+        showPanel("LOGIN_MENU");
+    }
+
+    /**
+     * Function: showPanel
+     * @param panelName
+     * @see new panel
+     *
+     * Hide all panels in the hashmap using for loop
+     * Create holding panel and set to new panel (Passed pram)
+     * Set new panel visible
+     */
+
+    public void showPanel(String panelName)
+    {
+        lastPanel = currentPanel;
+        currentPanel = panelName;
+
+        for (JPanel panel : mapPanels.values()) // Run through all panels in hashmap
+        {
+            panel.setVisible(false); // hide all panels in bufferpanel
+        }
+
+        JPanel panelToShow = mapPanels.get(panelName); // Create holding panel (panelToShow) and set equal to panelName (Passed Pram)
+        panelToShow.setVisible(true); // set new panel visible
+    }
+}
---
+++ src/java/View/LaunchApp.java
@@ -0,0 +1,57 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LaunchApp {
+
+    public static JFrame frame = new JFrame();
+    static BufferMenu bufferMenu = new BufferMenu();
+    public static final int Window_Width = 700;
+    public static final int Window_Height = 500;
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            createAndShowGUI(); // Method Call to create program.
+        });
+    }
+
+    private static void createAndShowGUI(){
+        frame.setSize(Window_Width,Window_Height); //sets size restraints
+        frame.setMinimumSize(new Dimension(Window_Width,Window_Height));
+        frame.setTitle(".:LBMS:.");
+        //frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // End program when 'x' is Clicked.
+
+
+        bufferMenu.setSize(frame.getWidth(),frame.getHeight());
+
+        frame.getContentPane().add(bufferMenu);
+        //frame.getContentPane().setBackground(Color.WHITE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+    public static void showLoginMenu() {
+        System.out.println("\n1 - Login");
+        System.out.println("2 - Sign Up");
+        System.out.println("3 - Exit");
+    }
+
+    public static void showMainMenu() {
+        System.out.println("\n1 - Library Book Search");
+        System.out.println("2 - Borrow Book");
+        System.out.println("3 - Find Borrowed Books");
+        System.out.println("4 - Return Book");
+        System.out.println("5 - Pay Fine");
+    }
+
+    public static void showAdminMenu() {
+        System.out.println("\n1 - Book Store Search");
+        System.out.println("2 - Book Purchase");
+        System.out.println("3 - Library Statistics Report");
+        System.out.println("4 - Advance Time");
+    }
+}
